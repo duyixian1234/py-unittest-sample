adafe0cd89bdfd0e0b8c2a253fee0cdb9454dfa5
Yixian Du <duyixian1234@qq.com>
2021-05-24 14:29:13 +0800

    set default values

--- app/config.py
+++ app/config.py
@@ -3,10 +3,10 @@ import os
 
 
 class Settings(BaseSettings):
-    APP_KEY: str = os.getenv("APP_KEY")
-    APP_SECRET: str = os.getenv("APP_SECRET")
-    API_DOMAIN: str = os.getenv("API_DOMAIN")
-    REDIRECT: str = "/admin"
+    APP_KEY: str = os.getenv("APP_KEY", "YOUR_APP_KEY")
+    APP_SECRET: str = os.getenv("APP_SECRET", "YOUR_APP_KEY")
+    API_DOMAIN: str = os.getenv("API_DOMAIN", "YOUR_APP_DOMAIN")
+    REDIRECT: str = os.getenv("REDIRECT", "/admin")
 
 
 settings = Settings()
